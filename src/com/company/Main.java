1a31fca856357c6116bd33a5a097ee4695f61c90
N1KF0X <96484000+N1KF0X@users.noreply.github.com>
2022-06-15 17:25:52 +0300

    init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile("\\b([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\b");
+        Matcher matcher = pattern.matcher(input);
+
+        if(matcher.find()) {
+            System.out.println(matcher.group());
+        }
+        else {
+            System.out.println("Не найдено");
+        }
+    }
+}
